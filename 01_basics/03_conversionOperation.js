d78ff5c6e7f32c4f87b3577a6d83f245d4cc4465
adityagaur02 <117304253+adityagaur02@users.noreply.github.com>
2025-05-09 11:22:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*   
+while converting things to number it gives i/p => o/p
+"33" => 33
+"33abc" => NaN
+true => 1;   false => 0;
+null => 0
+undefined => NaN
+*/
+
+let isLoggedIn = "aditya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+while converting to boolean
+1 => true; 0=> false;
+"" => false
+"aditya" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
