56dca4ba7665536cc02f49d8a25e053558013d87
adityagaur02 <117304253+adityagaur02@users.noreply.github.com>
2025-05-09 12:04:37 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+                                        // its not recommended to compare like these 6 as it converts null to 0 sometime in comparison
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);   // o/p => false as it checks data type also
