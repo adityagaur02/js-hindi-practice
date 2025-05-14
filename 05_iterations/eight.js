3a77523e61d28e987393a48e55eb87bea2ac4668
adityagaur02 <117304253+adityagaur02@users.noreply.github.com>
2025-05-14 12:08:06 +0000

    reduce

---
+++ 05_iterations/eight.js
@@ -0,0 +1,37 @@
+const myNums = [1, 2, 3]
+/*
+   Here accumulator(acc) takes start value which is passed (0 in this case) and then takes result as next value and,
+   currentValue(currVal) takes the current value from array i.e. 1,2,3
+*/
+// const myTotal = myNums.reduce(function (acc, currVal) {
+//     console.log(`acc: ${acc} and currVal: ${currVal}`);
+//     return acc + currVal
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, currVal) => acc + currVal,0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price,0)
+
+console.log(priceToPay);
